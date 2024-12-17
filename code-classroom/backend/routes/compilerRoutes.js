20f6204fb7a5cf27faf74f32e9edc253f2cf839e
SONIYA YETE <127082701+SONIYAYETE@users.noreply.github.com>
2024-12-17 23:55:35 +0530

    Delete code-classroom directory

--- code-classroom/backend/routes/compilerRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const authMiddleware = require('../middlewares/authMiddleware.js');
-const router = express.Router();
-const { compiler } = require('../controllers/compilerController.js')
-
-router.post('/', compiler)
-
-module.exports = router;
